12d9a85cdc8327bc15163e61d8206046ef8d5ce7
martinetorres <tmartineduardo@gmail.com>
2022-12-10 12:07:38 -0300

    Fix slider indentation

--- components/slider/index.tsx
+++ components/slider/index.tsx
@@ -8,51 +8,51 @@ import ImageIcon from "../ui/imageIcon";
 export default function Slider() {
     return(
         <Container>
-                <Image 
-                    boxSize='100%'
-                    objectFit='cover'
-                    src='/img/main_background.png' 
-                    alt='Featured movie'
-                    position='absolute'
-                    left={0}
-                />
+            <Image 
+                boxSize='100%'
+                objectFit='cover'
+                src='/img/main_background.png' 
+                alt='Featured movie'
+                position='absolute'
+                left={0}
+            />
 
-                <Flex direction='row' 
-                    position='absolute' 
-                    alignItems='flex-end' 
-                    boxSize='100%'
-                    justifyContent='space-between'>
+            <Flex direction='row' 
+                position='absolute' 
+                alignItems='flex-end' 
+                boxSize='100%'
+                justifyContent='space-between'>
 
-                    <Box paddingLeft={50} paddingBottom={80}>
-                        <Heading as='h4'>
-                            Original de <b>Liteflix</b>
-                        </Heading>
-                        <Heading as='h2' colorScheme={'brand'} textTransform={'uppercase'}>
-                            La casa de papel
-                        </Heading>
+                <Box paddingLeft={50} paddingBottom={80}>
+                    <Heading as='h4'>
+                        Original de <b>Liteflix</b>
+                    </Heading>
+                    <Heading as='h2' colorScheme={'brand'} textTransform={'uppercase'}>
+                        La casa de papel
+                    </Heading>
 
-                        <Flex direction='row' alignItems='flex-start' justifyContent='flex-start' gap={20} marginTop={10}>
-                            <ButtonWithIcon 
-                                text='Reproducir' 
-                                variant="primary" 
-                                icon={<ImageIcon boxSize={25} img='play' />} 
-                            />
+                    <Flex direction='row' alignItems='flex-start' justifyContent='flex-start' gap={20} marginTop={10}>
+                        <ButtonWithIcon 
+                            text='Reproducir' 
+                            variant="primary" 
+                            icon={<ImageIcon boxSize={25} img='play' />} 
+                        />
 
-                            <ButtonWithIcon 
-                                text='Mi lista' 
-                                variant="secondary" 
-                                hasBorder={true} 
-                                icon={<ImageIcon boxSize={25} img='plus' />} 
-                            />
-                        </Flex>
-                    </Box>
-                    
-                    <Box paddingRight={50} alignSelf='center'>
-                        <MoviesDropdown />
-                        <FeaturedMovies />
-                    </Box>
+                        <ButtonWithIcon 
+                            text='Mi lista' 
+                            variant="secondary" 
+                            hasBorder={true} 
+                            icon={<ImageIcon boxSize={25} img='plus' />} 
+                        />
+                    </Flex>
+                </Box>
+                
+                <Box paddingRight={50} alignSelf='center'>
+                    <MoviesDropdown />
+                    <FeaturedMovies />
+                </Box>
 
-                </Flex>
+            </Flex>
         </Container>
     )
 }
