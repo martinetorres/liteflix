6565489d67cf5debb8063a45a65086d894e96c01
martinetorres <tmartineduardo@gmail.com>
2022-12-09 21:06:33 -0300

    Add movies thumbnails hover styles w/o animations

--- components/movieThumbnail/index.tsx
+++ components/movieThumbnail/index.tsx
@@ -1,7 +1,11 @@
-import { Container, Flex, Heading, Image } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading, Image } from "@chakra-ui/react";
+import { useState } from "react";
 import RoundedButton from "../roundedButton";
+import { StarIcon } from "@chakra-ui/icons";
 
 export default function MovieThumbnail() {
+    const [isHovered, setIsHovered] = useState(false);
+
     return(
         <Container position='relative'>
             <Image 
@@ -11,12 +15,38 @@ export default function MovieThumbnail() {
                 h={146}
             />
 
-            <Flex position='absolute' top={0} left={0} boxSize='100%' alignItems='center' justifyContent='flex-end' direction='column'>
-                <RoundedButton />
+            <Flex 
+            position='absolute' 
+            paddingTop={isHovered ? 55 : 50} 
+            top={0}
+            left={0} 
+            boxSize='100%' 
+            alignItems='center' 
+            justifyContent={isHovered ? 'center' : 'flex-end'} 
+            direction={isHovered ? 'row' : 'column'}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            flexWrap={isHovered ? 'wrap' : 'nowrap'}
+            cursor='pointer'>
 
-                <Heading as='h5' marginTop={20} marginBottom={15}>
+                <RoundedButton size={isHovered ? 30 : 40} />
+
+                <Heading as='h5' marginTop={20} marginBottom={15} marginLeft={isHovered ? 5 : 0}>
                     House of cards
                 </Heading>
+
+                <Box position='relative' boxSize='100%'>
+                    <Flex position='absolute' top={5} left={0} display={isHovered ? 'flex' : 'none'} direction='row' justifyContent='space-between' w='100%' alignItems='center' color='white' paddingLeft={10} paddingRight={10}>
+                        <Box display='inline-block' fontSize={14}>
+                            <StarIcon boxSize={12} color='#64EEBC' marginRight={5} />
+                            7
+                        </Box>
+                        <Box display='inline-block' fontSize={14}>
+                            2020
+                        </Box>
+                    </Flex>
+                </Box>
+
             </Flex>
         </Container>
     )
--- components/roundedButton/index.tsx
+++ components/roundedButton/index.tsx
@@ -1,14 +1,14 @@
 import { Box } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
 
-export default function RoundedButton() {
+export default function RoundedButton({size} : {size: number}) {
     return (
         <Box>
             <AddIcon
-                boxSize={40} 
+                boxSize={size} 
                 borderRadius='50%' 
                 backgroundColor='#24242450' 
-                padding={12}  
+                padding={size / 4}  
                 border='1px solid #fff' 
                 color='#fff' 
             />
