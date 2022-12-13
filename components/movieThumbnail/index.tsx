61c1ab86b5d2668be54eb2e575573f09fe5c2a87
martinetorres <tmartineduardo@gmail.com>
2022-12-13 01:39:42 -0300

    Popular movies hover styles improved:

--- components/movieThumbnail/index.tsx
+++ components/movieThumbnail/index.tsx
@@ -26,14 +26,29 @@ export default function MovieThumbnail(
             onMouseLeave={() => setIsHovered(false)}
             cursor='pointer'>
             
-            <Image 
-                objectFit='cover'
-                src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
-                w={220}
-                h={146}
-                boxShadow='0px 0px 12px #24242470'
-                borderRadius={4}
-            />
+            <Box w={220} h={146}>
+                <Box 
+                    position='absolute' 
+                    boxSize='100%' 
+                    top={0} 
+                    left={0}
+                    background={ 
+                        isHovered ?
+                            '#242424bb' :
+                            'linear-gradient(180deg, rgba(0, 0, 0, 0) 22.78%, #000000 122.69%);'
+                    }
+                    transition='background 200ms ease-in'
+                />
+
+                <Image 
+                    objectFit='cover'
+                    src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
+                    boxShadow='0px 0px 12px #24242470'
+                    borderRadius={4}
+                    w='100%'
+                    h='100%'
+                />
+            </Box>
 
             <Flex 
             position='absolute' 
